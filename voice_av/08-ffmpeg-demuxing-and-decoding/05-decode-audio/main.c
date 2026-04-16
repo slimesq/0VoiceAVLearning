2905197f026b0ec814cb5ca56e22083bd8008892
bit <2656585116@qq.com>
2026-04-16 18:42:36 +0800

    [fix]add decode-audio

--- voice_av/08-ffmpeg-demuxing-and-decoding/05-decode-audio/main.c
+++ voice_av/08-ffmpeg-demuxing-and-decoding/05-decode-audio/main.c
@@ -10,6 +10,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <cerrno>
+#include "libavcodec/codec.h"
+#include "libavcodec/packet.h"
 
 #define AUDIO_INBUF_SIZE 20480
 #define AUDIO_REFILL_THRESH 4096
@@ -30,86 +33,12 @@ static void print_sample_format(AVFrame const* frame)
 
 static void decode(AVCodecContext* dec_ctx, AVPacket* pkt, AVFrame* frame, FILE* outfile)
 {
-    int i, ch;
-    int ret, data_size;
-    /* send the packet with the compressed data to the decoder */
-    ret = avcodec_send_packet(dec_ctx, pkt);
-    if (ret == AVERROR(EAGAIN))
-    {
-        fprintf(stderr,
-                "Receive_frame and send_packet both returned EAGAIN, which is an API violation.\n");
-    }
-    else if (ret < 0)
-    {
-        fprintf(stderr,
-                "Error submitting the packet to the decoder, err:%s, pkt_size:%d\n",
-                av_get_err(ret),
-                pkt->size);
-        return;
-    }
-
-    /* read all the output frames (infile general there may be any number of them */
-    while (ret >= 0)
-    {
-        // 对于frame, avcodec_receive_frame内部每次都先调用
-        ret = avcodec_receive_frame(dec_ctx, frame);
-        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
-        {
-            return;
-        }
-        else if (ret < 0)
-        {
-            fprintf(stderr, "Error during decoding\n");
-            exit(1);
-        }
-        data_size = av_get_bytes_per_sample(dec_ctx->sample_fmt);
-        if (data_size < 0)
-        {
-            /* This should not occur, checking just for paranoia */
-            fprintf(stderr, "Failed to calculate data size\n");
-            exit(1);
-        }
-        static int s_print_format = 0;
-        if (s_print_format == 0)
-        {
-            s_print_format = 1;
-            print_sample_format(frame);
-        }
-        /**
-            P表示Planar（平面），其数据格式排列方式为 :
-            LLLLLLRRRRRRLLLLLLRRRRRRLLLLLLRRRRRRL...（每个LLLLLLRRRRRR为一个音频帧）
-            而不带P的数据格式（即交错排列）排列方式为：
-            LRLRLRLRLRLRLRLRLRLRLRLRLRLRLRLRLRLRL...（每个LR为一个音频样本）
-         播放范例：   ffplay -ar 48000 -ac 2 -f f32le believe.pcm
-          */
-        int const channels = frame->ch_layout.nb_channels;
-        for (i = 0; i < frame->nb_samples; i++)
-        {
-            for (ch = 0; ch < channels; ch++)  // 交错的方式写入, 大部分float的格式输出
-            {
-                fwrite(frame->data[ch] + data_size * i, 1, data_size, outfile);
-            }
-        }
-    }
 }
 // 播放范例：   ffplay -ar 48000 -ac 2 -f f32le believe.pcm
 int main(int argc, char** argv)
 {
     char const* outfilename;
     char const* filename;
-    AVCodec const* codec;
-    AVCodecContext* codec_ctx = NULL;
-    AVCodecParserContext* parser = NULL;
-    int len = 0;
-    int ret = 0;
-    FILE* infile = NULL;
-    FILE* outfile = NULL;
-    uint8_t inbuf[AUDIO_INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
-    uint8_t* data = NULL;
-    size_t data_size = 0;
-    AVPacket* pkt = NULL;
-    AVFrame* decoded_frame = NULL;
-
     if (argc <= 2)
     {
         fprintf(stderr, "Usage: %s <input file> <output file>\n", argv[0]);
@@ -117,8 +46,23 @@ int main(int argc, char** argv)
     }
     filename = argv[1];
     outfilename = argv[2];
+    FILE* infile = NULL;
+    fopen_s(&infile, filename, "rb");
+    if (!infile)
+    {
+        printf("fopen_s infile error\n");
+        return -1;
+    }
+    FILE* outfile = NULL;
+    fopen_s(&outfile, outfilename, "wb");
+    if (!outfile)
+    {
+        printf("fopen_s outfile error\n");
+        return -1;
+    }
 
-    pkt = av_packet_alloc();
+    uint8_t inbuf[AUDIO_INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
+
     enum AVCodecID audio_codec_id = AV_CODEC_ID_AAC;
     if (strstr(filename, "aac") != NULL)
     {
@@ -133,112 +77,62 @@ int main(int argc, char** argv)
         printf("default codec id:%d\n", audio_codec_id);
     }
 
-    // 查找解码器
-    codec = avcodec_find_decoder(audio_codec_id);  // AV_CODEC_ID_AAC
-    if (!codec)
-    {
-        fprintf(stderr, "Codec not found\n");
-        exit(1);
-    }
-    // 获取裸流的解析器 AVCodecParserContext(数据)  +  AVCodecParser(方法)
-    parser = av_parser_init(codec->id);
+    AVCodecParserContext* parser = av_parser_init(audio_codec_id);
     if (!parser)
     {
-        fprintf(stderr, "Parser not found\n");
-        exit(1);
-    }
-    // 分配codec上下文
-    codec_ctx = avcodec_alloc_context3(codec);
-    if (!codec_ctx)
-    {
-        fprintf(stderr, "Could not allocate audio codec context\n");
-        exit(1);
+        printf("av_parser_init error");
+        return -1;
     }
 
-    // 将解码器和解码器上下文进行关联
-    if (avcodec_open2(codec_ctx, codec, NULL) < 0)
+    AVCodec const* decoder = avcodec_find_decoder(audio_codec_id);
+    if (!decoder)
     {
-        fprintf(stderr, "Could not open codec\n");
-        exit(1);
+        printf("avcodec_find_decoder error");
+        return -1;
+    }
+    AVCodecContext* codec = avcodec_alloc_context3(decoder);
+    if (!codec)
+    {
+        printf("avcodec_alloc_context3 error");
+        return -1;
+    }
+    if (avcodec_open2(codec, decoder, NULL) < 0)
+    {
+        printf("avcodec_open2 error");
+        return -1;
     }
 
-    // 打开输入文件
-    infile = fopen(filename, "rb");
-    if (!infile)
+    AVPacket* pkt = av_packet_alloc();
+    if (!pkt)
     {
-        fprintf(stderr, "Could not open %s\n", filename);
-        exit(1);
+        printf("av_packet_alloc error");
+        return -1;
     }
-    // 打开输出文件
-    outfile = fopen(outfilename, "wb");
-    if (!outfile)
-    {
-        avcodec_free_context(&codec_ctx);
-        exit(1);
-    }
+    AVFrame* frame = av_frame_alloc();
 
-    // 读取文件进行解码
-    data = inbuf;
-    data_size = fread(inbuf, 1, AUDIO_INBUF_SIZE, infile);
-
-    while (data_size > 0)
+    int data_size = fread(inbuf, AUDIO_INBUF_SIZE, 1, infile);
+    while (1)
     {
-        if (!decoded_frame)
+        int parseSize = av_parser_parse2(parser,
+                                         codec,
+                                         &pkt->data,
+                                         &pkt->size,
+                                         inbuf,
+                                         data_size,
+                                         AV_NOPTS_VALUE,
+                                         AV_NOPTS_VALUE,
+                                         0);
+        if (!frame)
         {
-            if (!(decoded_frame = av_frame_alloc()))
-            {
-                fprintf(stderr, "Could not allocate audio frame\n");
-                exit(1);
-            }
+            frame = av_frame_alloc();
         }
+        decode(codec, pkt, frame, outfile);
 
-        ret = av_parser_parse2(parser,
-                               codec_ctx,
-                               &pkt->data,
-                               &pkt->size,
-                               data,
-                               data_size,
-                               AV_NOPTS_VALUE,
-                               AV_NOPTS_VALUE,
-                               0);
-        if (ret < 0)
-        {
-            fprintf(stderr, "Error while parsing\n");
-            exit(1);
-        }
-        data += ret;       // 跳过已经解析的数据
-        data_size -= ret;  // 对应的缓存大小也做相应减小
-
-        if (pkt->size)
-        {
-            decode(codec_ctx, pkt, decoded_frame, outfile);
-        }
-
-        if (data_size < AUDIO_REFILL_THRESH)  // 如果数据少了则再次读取
-        {
-            memmove(inbuf, data, data_size);  // 把之前剩的数据拷贝到buffer的起始位置
-            data = inbuf;
-            // 读取数据 长度: AUDIO_INBUF_SIZE - data_size
-            len = fread(data + data_size, 1, AUDIO_INBUF_SIZE - data_size, infile);
-            if (len > 0)
-            {
-                data_size += len;
-            }
-        }
+        data_size -= parseSize;
+        memmove(inbuf, inbuf + parseSize, data_size);
+        int readSize = fread(inbuf + data_size, AUDIO_INBUF_SIZE - data_size, 1, infile);
+        data_size += readSize;
     }
-
-    /* 冲刷解码器 */
-    pkt->data = NULL;  // 让其进入drain mode
-    pkt->size = 0;
-    decode(codec_ctx, pkt, decoded_frame, outfile);
-
-    fclose(outfile);
-    fclose(infile);
-
-    avcodec_free_context(&codec_ctx);
-    av_parser_close(parser);
-    av_frame_free(&decoded_frame);
-    av_packet_free(&pkt);
 
     printf("main finish, please enter Enter and exit\n");
     return 0;
